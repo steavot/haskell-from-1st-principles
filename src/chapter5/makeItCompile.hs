bcfd1ea73c7bd54d7d202b2923f3e086bea72a5e
steavot <tomsteavenson@gmail.com>
2018-09-09 16:50:12 +0100

    Chapter5

---
+++ src/chapter5/makeItCompile.hs
@@ -0,0 +1,20 @@
+module MakeItCompile where
+
+-- 1
+bigNum = (^) 5
+wahoo = bigNum 10
+
+-- 2
+x = print
+y = print "woohoo!"
+z = x "hello world"
+
+-- 3
+a = (+)
+b = 5
+c = a b 10
+d = a c 200
+
+-- 4
+e = 12 + f
+f = 10000
